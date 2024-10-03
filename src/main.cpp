9f9aa688711dd381c95b1518083f7b3d2ee9956e
TemZ <110783208+tems2548@users.noreply.github.com>
2024-10-03 18:28:19 +0700

    ....

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include "PCF8574.h"
 
 #define range 15        //range to detected
+#define read_light_pin A0
 #define SCREEN_WIDTH 128    // OLED display width, in pixels
 #define SCREEN_HEIGHT 32    // OLED display height, in pixels
 #define OLED_RESET -1       // Reset pin # (or -1 if sharing Arduino reset pin)
@@ -34,7 +35,7 @@ enum states
 states state;
  void RGBpcf_begin(uint8_t pin1,uint8_t pin2,uint8_t pin3);
  void RGB_pcf(byte R, byte G, byte B);
- void work(int dis1_value,int dis2_value);
+ void work(int dis1_value,int dis2_value,int light_val);
 };
 void task::RGBpcf_begin(uint8_t pin1,uint8_t pin2,uint8_t pin3){
   pcf8574.pinMode(pin1,OUTPUT);
@@ -54,11 +55,11 @@ void task::RGB_pcf(byte R, byte G, byte B)
   pcf8574.digitalWrite(green_pcf, G);
   pcf8574.digitalWrite(blue_pcf, B);
 }
-void task::work(int dis1_value,int dis2_value){
+void task::work(int dis1_value,int dis2_value,int light_val){
   switch (state)
   {
   case idle:
-    if (light <= 25)
+    if (light_val <= 25)
     {
       RGB_pcf(0, 0, 0);
       if (dis1_value <= range || dis2_value <= range)
@@ -144,6 +145,8 @@ Sensors ultra_sonic_1;
 Sensors ultra_sonic_2;
 Sensors ultra_sonic_3;
 Sensors ultra_sonic_4;
+Sensors upper_US_1;
+Sensors upper_US_2;
 
 task park1;
 task park2;
@@ -156,6 +159,10 @@ void setup()
   ultra_sonic_2.begin(12, 11);
   ultra_sonic_3.begin(6, 7);
   ultra_sonic_4.begin(4, 5);
+
+  upper_US_1.begin(2,3);
+  upper_US_2.begin(8,13);
+
   
   pcf8574.begin();
   display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS);
@@ -175,20 +182,21 @@ void loop()
 {
   display.setTextSize(2);
   display.setCursor(0, 0);
-
+  int LIGHT = map(analogRead(read_light_pin),0, 1023, 100, 0);
   //!parking lot 1
   int dis1 = ultra_sonic_1.GetDistance();
   int dis2 = ultra_sonic_2.GetDistance();
-  park1.work(dis1,dis2);
+  int detect_park1 = upper_US_1.GetDistance();
+  park1.work(dis1,dis2,LIGHT);
 
   //!parking lot 2
   int dis3 = ultra_sonic_3.GetDistance();
   int dis4 = ultra_sonic_4.GetDistance();
-  park2.work(dis3,dis4);
-
-  light = map(analogRead(A0), 0, 1024, 100, 0);
-  Serial.print("distance 3 = " + String(dis3) + " / distance 4 = " + String(dis4));
+  int detect_park2 = upper_US_2.GetDistance();
+  park2.work(dis3,dis4,LIGHT);
   
+  //Serial.print("distance up 1 = " + String(detect_park1) + " / distance up 2 = " + String(detect_park1));
+  Serial.print("light = " + String(LIGHT));
   delay(40);
   Serial.println("");
   display.display();
